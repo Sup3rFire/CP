9fe34f9a662154d2a5ca765cec91f0a8c94409be
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2023-09-12 11:10:19 +0700

    editorialed

--- loaling/A_The_Good_Array.cpp
+++ loaling/A_The_Good_Array.cpp
@@ -17,20 +17,6 @@ using namespace std;
 
 const ld EPS = 1e-9;
 const ld PI = 3.141592653589793238462643383279502884197169399375105820974944;
-bool a[105];
-int n, k;
-bool check(int x) {
-  int cnt = 0;
-  for (int i = 1; i <= x; i++) {
-    if (a[i]) cnt++;
-  }
-  if ((x+k-1)/k > cnt) return false;
-  return true;
-}
-
-int mirror(int x) {
-  return n-x+1;
-}
 
 int main () {
   ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
@@ -38,17 +24,9 @@ int main () {
   int t;
   cin >> t;
   for (int i = 0; i < t; i++) {
+    int n, k;
     cin >> n >> k;
-    memset(a, 0, sizeof(a));
-    for (int i = 1; i <= n; i++) {
-      if (!check(i)) a[i] = a[mirror(i)] = 1;
-    }
-
-    int cnt = 0;
-    for (int i = 1; i <= n; i++) {
-      cnt += a[i];
-    }
-    cout << cnt << endl;  
+    cout << (n-2+k)/k+1 << endl;
   }
   
   return 0;
