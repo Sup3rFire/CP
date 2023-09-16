9b8e4da5c1c58a8faddf06eb421d9bd0bf5e6524
SuperFire <36259493+Sup3rFire@users.noreply.github.com>
2023-09-16 07:42:08 +0700

    loaling

---
+++ loaling/A_Haiku.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// loal is a cute girl
+
+#define ll long long
+#define ld long double
+#define pow2(x) (x)*(x)
+#define le left
+#define ri right
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define isvowel(x) (x) == 'a' || (x) == 'i' || (x) == 'u' || (x) == 'e' || (x) == 'o'
+
+const ld EPS = 1e-9;
+const ld PI = 3.141592653589793238462643383279502884197169399375105820974944;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  
+  string loal, is, girl;
+  getline(cin, loal); getline(cin, is); getline(cin, girl);
+
+  int cute = 0;
+  for (auto girl: loal) {
+    if (isvowel(girl)) {
+      cute++;
+    }
+  }
+  if (cute != 5) {
+    cout << "NO";
+    return 0;
+  }
+
+  cute = 0;
+  for (auto loalgirl: is) {
+    if (isvowel(loalgirl)) {
+      cute++;
+    }
+  }
+  if (cute != 7) {
+    cout << "NO";
+    return 0;
+  }
+
+  cute = 0;
+  for (auto loal: girl) {
+    if (isvowel(loal)) {
+      cute++;
+    }
+  }
+  if (cute != 5) {
+    cout << "NO";
+    return 0;
+  }
+
+  cout << "YES";
+
+  return 0;
+}
