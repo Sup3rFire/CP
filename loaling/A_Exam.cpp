b68275e6edf7c3473da226088272fd24bdec3adb
SuperFire <36259493+Sup3rFire@users.noreply.github.com>
2023-09-30 21:02:53 +0700

    loal girl

---
+++ loaling/A_Exam.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// loal is a cute girl
+
+#define ll long long
+#define ld long double
+#define pow2(x) (x)*(x)
+#define le left
+#define ri right
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define isvowel(x) (x) == 'a' || (x) == 'i' || (x) == 'u' || (x) == 'e' || (x) == 'o'
+
+const ld EPS = 1e-9;
+const ld PI = 3.141592653589793238462643383279502884197169399375105820974944;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  
+  int n;
+  cin >> n;
+
+  if (n < 4) {
+    if (n == 3) {
+      cout << 2 << endl << "1 3";
+    } else {
+      cout << 1 << endl << 1;
+    }
+  } else {
+    cout << n << endl;
+    for (int i = 2; i <= n; i += 2) cout << i << " ";
+    for (int i = 1; i <= n; i += 2) cout << i << " ";
+  }
+  
+  return 0;
+}
