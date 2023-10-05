7a1b4515e8de3b1b9eaca0c407a2abb974837b21
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2023-10-05 07:09:19 +0700

    loal girl

---
+++ loaling/A_Vasya_and_Digital_Root.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// loal is a cute girl
+
+#define ll long long
+#define ld long double
+#define pow2(x) (x)*(x)
+#define le left
+#define ri right
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define isvowel(x) (x) == 'a' || (x) == 'i' || (x) == 'u' || (x) == 'e' || (x) == 'o'
+
+const ld EPS = 1e-9;
+const ld PI = 3.141592653589793238462643383279502884197169399375105820974944;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  
+  int loal, girl;
+  cin >> loal >> girl;
+  if (girl == 0 && loal > 1) {cout << "No solution"; return 0;}
+  cout << girl;
+  for (int cute = 1; cute < loal; cute++) cout << 0;
+  
+  return 0;
+}
