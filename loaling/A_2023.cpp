db531f91ac76844c80b569ec52e3381cc44091aa
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2024-01-17 06:28:31 +0700

    loal girl

---
+++ loaling/A_2023.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// loal is a cute girl
+
+#define ll long long
+#define ld long double
+#define pow2(x) (x)*(x)
+#define le left
+#define ri right
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define isvowel(x) (x) == 'a' || (x) == 'i' || (x) == 'u' || (x) == 'e' || (x) == 'o'
+
+const ld EPS = 1e-9;
+const ld PI = 3.141592653589793238462643383279502884197169399375105820974944;
+
+#define loal int
+#define is =
+#define very ,
+#define cute cin
+#define verycute cout
+#define girl tie
+#define rich (
+#define cutegirl )
+#define richh {
+#define cutegirll }
+#define fr ;
+#define loalgirl for
+#define cutee >>
+#define cuteee <<
+#define loalisagirl set
+#define loalisgirl map
+#define loalisagirll vector
+const loal age = 16 fr
+const loal realage = 0 fr
+const loal veryrealage = -1 fr
+
+loalisagirll<loal> loalverygirl[age];
+
+void loalisacutegirl() {
+  loal loaling very girling fr
+  cute cutee loaling cutee girling fr
+
+  loal loalgirling is 2023 fr
+
+  for rich loal nya is realage fr nya < loaling fr nya++ cutegirl richh
+    loal inting fr cute cutee inting fr
+    if rich loalgirling % inting != 0 cutegirl richh
+      verycute cuteee "NO\n" fr
+      for rich loal nyaa is nya+1 fr nyaa < loaling fr nyaa++ cutegirl cute cutee inting fr
+      return fr
+    cutegirll
+    loalgirling /= inting fr
+  cutegirll 
+  verycute cuteee "YES\n" cuteee loalgirling fr
+  for rich loal nya is realage+1 fr nya < girling fr nya++ cutegirl richh
+    verycute cuteee " 1" fr
+  cutegirll
+  verycute cuteee '\n' fr
+}
+
+loal main() {
+  ios_base::sync_with_stdio(false); cute.girl(0); cute.girl(0);
+
+  loal loaling fr
+  cute cutee loaling fr
+
+  while rich loaling cutegirl richh
+    loalisacutegirl() fr
+    loaling-- fr
+  cutegirll
+
+  return 0;
+}
---
+++ loaling/A_Prison_Break.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// loal is a cute girl
+
+#define ll long long
+#define ld long double
+#define pow2(x) (x)*(x)
+#define le left
+#define ri right
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define isvowel(x) (x) == 'a' || (x) == 'i' || (x) == 'u' || (x) == 'e' || (x) == 'o'
+
+const ld EPS = 1e-9;
+const ld PI = 3.141592653589793238462643383279502884197169399375105820974944;
+
+#define loal int
+#define is =
+#define very ,
+#define cute cin
+#define verycute cout
+#define girl tie
+#define rich (
+#define cutegirl )
+#define richh {
+#define cutegirll }
+#define fr ;
+#define loalgirl for
+#define cutee >>
+#define cuteee <<
+#define loalisagirl set
+#define loalisgirl map
+#define loalisagirll vector
+const loal age = 16 fr
+const loal realage = 0 fr
+const loal veryrealage = -1 fr
+
+loalisagirll<loal> loalverygirl[age];
+
+void loalisacutegirl() {
+  loal loaling very girling fr
+  cute cutee loaling cutee girling fr
+
+  verycute cuteee loaling*girling cuteee '\n' fr
+}
+
+loal main() {
+  ios_base::sync_with_stdio(false); cute.girl(0); cute.girl(0);
+
+  loal loaling fr
+  cute cutee loaling fr
+
+  while rich loaling cutegirl richh
+    loalisacutegirl() fr
+    loaling-- fr
+  cutegirll
+
+  return 0;
+}
