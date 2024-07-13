3c50a4ff0d0ba2965f8c40f38d3e52430c2eae37
SuperFire <36259493+Sup3rFire@users.noreply.github.com>
2024-07-13 18:24:23 +0700

    loal girl

---
+++ Codeforces/Codeforces_Codeforces_Round_957_Div_3/C_Gorilla_and_Permutation.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// loal is the cutest girl
+
+#define ll long long
+#define ld long double
+#define pow2(x) (x)*(x)
+#define le left
+#define ri right
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define isvowel(x) (x) == 'a' || (x) == 'i' || (x) == 'u' || (x) == 'e' || (x) == 'o'
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+
+const ld EPS = 1e-9;
+const ld PI = 3.141592653589793238462643383279502884197169399375105820974944;
+
+void loalisaverycuteegirl() {
+  int n, m, k;
+  cin >> n >> m >> k;
+  for (int i = 0; n-i > m; i++) {
+    cout << n-i << ' ';
+  }
+  for (int i = 1; i <= m; i++) cout << i << ' ';
+
+  cout << '\n';
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  
+  int loal;
+  cin >> loal;
+  for (int cute = 0; cute < loal; cute++) loalisaverycuteegirl();
+  
+  return 0;
+}
